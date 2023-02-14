a2994a6621c1fab54afae194a74de5a7e586e4ce
OBERAPFEL1 <72599189+blockcrafter21@users.noreply.github.com>
2023-02-14 20:44:24 +0100

    VPlan page is working.
    TODO:
    
    1. Make it look better
    2. Exchange the demo url by a real one

--- src/pages/VPlanPage.tsx
+++ src/pages/VPlanPage.tsx
@@ -1,46 +1,94 @@
+import React, { useState, useEffect } from "react";
 import {
-    IonCard, IonCardContent, IonCardHeader, IonCardTitle,
-    IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRefresher, IonRefresherContent, IonRow, IonTitle, IonToolbar
-} from '@ionic/react';
-import React, { useState } from 'react';
-import { fetchData } from '../services/dataService';
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonRefresher,
+    IonRefresherContent,
+    IonCard,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel,
+} from "@ionic/react";
 
-const VPlanPage: React.FC = () => {
-    const [data, setData] = useState([]);
+interface VertretungsData {
+    [key: string]: {
+        [key: string]: {
+            stunde: string;
+            vertreter: string;
+            fach: string;
+            raum: string;
+            vertretungs_text: string;
+        };
+    };
+}
 
-    const handleRefresh = (event: CustomEvent) => {
-        fetchData().then((response) => {
-            setData(response);
-            event.detail.complete();
-        });
+const Vertretungsplan: React.FC = () => {
+    const [data, setData] = useState<VertretungsData>({});
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        fetch("https://jsonendpoint.com/my-unique/endpoint/x6j0t ")
+            .then((response) => response.json())
+            .then((data: VertretungsData) => {
+                setData(data);
+            });
+    };
+
+    const doRefresh = (event: CustomEvent) => {
+        fetchData();
+        event.detail.complete();
     };
 
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Data Page</IonTitle>
-                </IonToolbar>
+                <h1>Vertretungsplan</h1>
             </IonHeader>
             <IonContent>
-                <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
-                    <IonRefresherContent />
+                <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+                    <IonRefresherContent></IonRefresherContent>
                 </IonRefresher>
-                {data.map((item, index) => (
+                {Object.keys(data).map((day, index) => (
                     <IonCard key={index}>
-                        <IonCardHeader>
-                            <IonCardTitle>{item[0]}</IonCardTitle>
-                        </IonCardHeader>
                         <IonCardContent>
-                            {item[1]}
+                            <h2>{day}</h2>
+                            <IonList>
+                                {Object.keys(data[day]).map((klasse, index) => (
+                                    <IonItem key={index}>
+                                        <IonLabel>
+                                            <h3>{klasse}</h3>
+                                            <p>
+                                                <strong>Stunde:</strong> {data[day][klasse].stunde}
+                                            </p>
+                                            <p>
+                                                <strong>Vertreter:</strong>{" "}
+                                                {data[day][klasse].vertreter}
+                                            </p>
+                                            <p>
+                                                <strong>Fach:</strong> {data[day][klasse].fach}
+                                            </p>
+                                            <p>
+                                                <strong>Raum:</strong> {data[day][klasse].raum}
+                                            </p>
+                                            <p>
+                                                <strong>Vertretungs-text:</strong>{" "}
+                                                {data[day][klasse].vertretungs_text}
+                                            </p>
+                                        </IonLabel>
+                                    </IonItem>
+                                ))}
+                            </IonList>
                         </IonCardContent>
                     </IonCard>
                 ))}
-
-
             </IonContent>
         </IonPage>
     );
 };
 
-export default VPlanPage;
+export default Vertretungsplan;
