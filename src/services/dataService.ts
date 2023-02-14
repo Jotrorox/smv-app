e0c3a3b0aa277e4ccd5586ba39a1874c12df7b94
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2023-02-14 16:55:17 +0100

    Add VPlan json handling Kind of working, now need to make it handle more data and look good

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -2,9 +2,9 @@ export const fetchData = async () => {
     const response = await fetch('https://jsonendpoint.com/my-unique/endpoint/eohmd');
     const data = await response.json();
     console.log(data)
-    const formattedData = data.map((item: string[]) => [item[0], item[1]]);
+    const formattedData = data.rights.map((item: any) => [item[Object.keys(item)[0]], item[Object.keys(item)[1]]]);
     console.log(formattedData);
-    return data;
+    return formattedData;
 }
 
 
